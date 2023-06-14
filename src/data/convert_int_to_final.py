14b3d489e48ae0e440ff27d26a4ede28b8d46001
Vladislav_Laptev <vvl39@tpu.ru>
2023-06-14 14:56:03 +0700

    initialization project

---
+++ src/data/convert_int_to_final.py
@@ -0,0 +1,90 @@
+import logging
+import os
+import shutil
+from typing import List
+
+import cv2
+import hydra
+import numpy as np
+import pandas as pd
+import supervisely_lib as sly
+from joblib import Parallel, delayed
+from omegaconf import DictConfig, OmegaConf
+from sklearn.model_selection import train_test_split
+from tqdm import tqdm
+
+log = logging.getLogger(__name__)
+log.setLevel(logging.INFO)
+
+
+def get_mask(
+        img_path: str,
+        classes: List[str],
+        data: pd.DataFrame,
+        save_dir: str,
+) -> None:
+    if len(data) > 0 and len(list(set(classes) & set(data.class_name.unique()))) > 0:
+        mask = np.zeros((int(data.image_width.mean()), int(data.image_height.mean())))
+        for _, row in data.iterrows():
+            if row.class_name in classes:
+                figure_data = sly.Bitmap.base64_2_data(row.mask_b64)
+                mask[figure_data == True] = row.class_id + 1
+        cv2.imwrite(f'{save_dir}/mask/{os.path.basename(img_path)}', mask)
+        shutil.copy(img_path, f'{save_dir}/img/{os.path.basename(img_path)}')
+
+
+@hydra.main(
+    config_path=os.path.join(os.getcwd(), 'configs'),
+    config_name='convert_int_to_final',
+    version_base=None,
+)
+def main(cfg: DictConfig) -> None:
+    log.info(f'Config:\n\n{OmegaConf.to_yaml(cfg)}')
+
+    for subset in ['train', 'val']:
+        for dir_type in ['img', 'mask']:
+            os.makedirs(f'{cfg.save_dir}/{subset}/{dir_type}', exist_ok=True)
+
+    df = pd.read_excel(cfg.df_path)
+    images_path = np.unique(df.image_path.values)
+    train_data, test_data = train_test_split(
+        images_path,
+        train_size=cfg.train_size,
+        shuffle=True,
+        random_state=cfg.seed,
+    )
+
+    train_img_paths = df[df['image_path'].isin(train_data)].image_path.values
+    test_img_paths = df[df['image_path'].isin(test_data)].image_path.values
+    log.info(f'Train images...: {len(train_img_paths)}')
+    log.info(f'Test images....: {len(train_img_paths)}')
+
+    test_df = df.loc[df['image_path'].isin(test_img_paths)]
+    train_df = df.loc[df['image_path'].isin(train_img_paths)]
+
+    test_df.class_name.value_counts()
+    train_df.class_name.value_counts()
+
+    Parallel(n_jobs=-1, backend='threading')(
+        delayed(get_mask)(
+            img_path=img_path,
+            classes=cfg.classes,
+            data=df.loc[df['image_path'] == img_path],
+            save_dir=f'{cfg.save_dir}/train',
+        )
+        for img_path in tqdm(train_img_paths, desc='Preparing the training subset')
+    )
+
+    Parallel(n_jobs=-1, backend='threading')(
+        delayed(get_mask)(
+            img_path=img_path,
+            classes=cfg.classes,
+            data=df.loc[df['image_path'] == img_path],
+            save_dir=f'{cfg.save_dir}/val',
+        )
+        for img_path in tqdm(test_img_paths, desc='Preparing the testing subset')
+    )
+
+
+if __name__ == '__main__':
+    main()
---
+++ src/models/smp/train.py
@@ -0,0 +1,94 @@
+import datetime
+import logging
+import os
+
+import hydra
+import pytorch_lightning as pl
+from clearml import Task
+from omegaconf import DictConfig, OmegaConf
+from pytorch_lightning import loggers as pl_loggers
+from pytorch_lightning.callbacks import LearningRateMonitor, ModelCheckpoint
+
+from src.models.smp.dataset import OCTDataModule
+from src.models.smp.model import OCTSegmentationModel
+
+log = logging.getLogger(__name__)
+log.setLevel(logging.INFO)
+
+
+@hydra.main(
+    config_path=os.path.join(os.getcwd(), 'configs'),
+    config_name='train_smp_model',
+    version_base=None,
+)
+def main(cfg: DictConfig) -> None:
+    log.info(f'Config:\n\n{OmegaConf.to_yaml(cfg)}')
+
+    today = datetime.datetime.today()
+    model_dir = f'models/{cfg.project_name}/{cfg.task_name}_#{today.strftime("%m-%d-%H.%M")}'
+
+    # Initialize ClearML task
+    Task.init(
+        project_name=cfg.project_name,
+        task_name=cfg.task_name,
+        auto_connect_frameworks={'tensorboard': True, 'pytorch': True},
+    )
+
+    # Initialize data module
+    oct_data_module = OCTDataModule(
+        dataset_name=cfg.dataset_name,
+        project_name=cfg.project_name,
+        input_size=cfg.input_size,
+        classes=cfg.classes,
+        classes_idx=cfg.classes_idx,
+        batch_size=cfg.batch_size,
+        num_workers=os.cpu_count(),
+    )
+
+    # Initialize callbacks
+    checkpoint = ModelCheckpoint(
+        save_top_k=5,
+        monitor='val/loss',
+        mode='min',
+        dirpath=f'{model_dir}/ckpt/',
+        filename='models_{epoch:02d}',
+    )
+    lr_monitor = LearningRateMonitor(
+        logging_interval='epoch',
+        log_momentum=False,
+    )
+    tb_logger = pl_loggers.TensorBoardLogger(
+        save_dir='logs/',
+    )
+
+    # Initialize model
+    model = OCTSegmentationModel(
+        cfg.architecture,
+        cfg.encoder,
+        in_channels=3,
+        classes=cfg.classes,
+        colors=cfg.classes_color,
+    )
+
+    # Initialize and tun trainer
+    trainer = pl.Trainer(
+        devices=-1,
+        accelerator=cfg.device,
+        max_epochs=cfg.epochs,
+        logger=tb_logger,
+        callbacks=[
+            lr_monitor,
+            checkpoint,
+        ],
+        enable_checkpointing=True,
+        log_every_n_steps=cfg.batch_size,
+        default_root_dir=model_dir,
+    )
+    trainer.fit(
+        model,
+        datamodule=oct_data_module,
+    )
+
+
+if __name__ == '__main__':
+    main()
