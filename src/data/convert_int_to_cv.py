662fce63bb3bd89766e785702839b1239dc256d7
Viacheslav Danilov <viacheslav.v.danilov@gmail.com>
2024-02-28 16:41:15 +0100

    [Fix]: rework CV split pipeline

--- src/data/convert_int_to_cv.py
+++ src/data/convert_int_to_cv.py
@@ -10,6 +10,7 @@ from omegaconf import DictConfig, OmegaConf
 from sklearn.model_selection import KFold
 from tqdm import tqdm
 
+from src import PROJECT_DIR
 from src.data.convert_int_to_final import process_mask, process_metadata, save_metadata
 
 log = logging.getLogger(__name__)
@@ -20,10 +21,14 @@ def cross_validation_split(
     df: pd.DataFrame,
     id_column: str,
     num_folds: int,
+    seed: int,
 ) -> List[Tuple[np.ndarray, np.ndarray]]:
     ids = df[id_column].unique()
-    kf = KFold(n_splits=num_folds, shuffle=True, random_state=11)
-
+    kf = KFold(
+        n_splits=num_folds,
+        shuffle=True,
+        random_state=seed,
+    )
     splits = []
     for train_idx, val_idx in kf.split(ids):
         splits.append(
@@ -37,42 +42,48 @@ def cross_validation_split(
 
 
 @hydra.main(
-    config_path=os.path.join(os.getcwd(), 'configs'),
-    config_name='convert_int_to_cross_val',
+    config_path=os.path.join(PROJECT_DIR, 'configs'),
+    config_name='convert_int_to_cv',
     version_base=None,
 )
 def main(cfg: DictConfig) -> None:
     log.info(f'Config:\n\n{OmegaConf.to_yaml(cfg)}')
 
-    for _ in range(cfg.split_count):
+    # Define absolute paths
+    data_dir = os.path.join(PROJECT_DIR, cfg.data_dir)
+    save_dir = os.path.join(PROJECT_DIR, cfg.save_dir)
+
+    for fold_idx in range(cfg.num_folds):
         for subset in ['train', 'test']:
             for dir_type in ['img', 'mask', 'mask_color']:
-                os.makedirs(f'{cfg.save_dir}/split_{_}/{subset}/{dir_type}', exist_ok=True)
+                os.makedirs(f'{save_dir}/fold_{fold_idx+1}/{subset}/{dir_type}', exist_ok=True)
 
     # Read and process metadata
-    df_path = os.path.join(cfg.data_dir, 'metadata.csv')
-    df = pd.read_csv(df_path)
+    csv_path = os.path.join(data_dir, 'metadata.csv')
+    df = pd.read_csv(csv_path)
     df_filtered = process_metadata(df=df, classes=cfg.class_names)
 
-    # Split dataset
+    # Cross-validation split of the dataset
     splits = cross_validation_split(
         df=df_filtered,
         id_column=cfg.split_column,
-        num_folds=cfg.split_count,
+        num_folds=cfg.num_folds,
+        seed=cfg.seed,
     )
 
-    for _, (df_train, df_test) in enumerate(splits):
+    for fold_idx, (df_train, df_test) in enumerate(splits, start=1):
         # Save metadata
         save_metadata(
             df_train=df_train,
             df_test=df_test,
-            save_dir=f'{cfg.save_dir}/split_{_}',
+            save_dir=f'{save_dir}/fold_{fold_idx}',
         )
 
         gb_train = df_train.groupby('image_path')
         gb_test = df_test.groupby('image_path')
-        log.info(f'Train images...: {len(gb_train)}')
-        log.info(f'Test images....: {len(gb_test)}')
+        log.info('')
+        log.info(f'Fold {fold_idx} - Train images...: {len(gb_train)}')
+        log.info(f'Fold {fold_idx} - Test images....: {len(gb_test)}')
 
         # Process train and test subsets
         Parallel(n_jobs=-1, backend='threading')(
@@ -80,9 +91,9 @@ def main(cfg: DictConfig) -> None:
                 img_path=img_path,
                 df=df,
                 smooth_mask=cfg.smooth_mask,
-                save_dir=f'{cfg.save_dir}/split_{_}/train',
+                save_dir=f'{save_dir}/fold_{fold_idx}/train',
             )
-            for img_path, df in tqdm(gb_train, desc='Process train subset')
+            for img_path, df in tqdm(gb_train, desc=f'Process train subset - Fold {fold_idx}')
         )
 
         Parallel(n_jobs=-1, backend='threading')(
@@ -90,9 +101,9 @@ def main(cfg: DictConfig) -> None:
                 img_path=img_path,
                 df=df,
                 smooth_mask=cfg.smooth_mask,
-                save_dir=f'{cfg.save_dir}/split_{_}/test',
+                save_dir=f'{save_dir}/fold_{fold_idx}/test',
             )
-            for img_path, df in tqdm(gb_test, desc='Process test subset')
+            for img_path, df in tqdm(gb_test, desc=f'Process test subset - Fold {fold_idx}')
         )
 
 
