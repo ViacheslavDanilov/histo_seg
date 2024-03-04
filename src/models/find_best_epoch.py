4c200400e6e964fed7a7a171ae7d2600862ae6bb
Viacheslav Danilov <viacheslav.v.danilov@gmail.com>
2024-03-04 18:02:11 +0100

    [Fix]: optimize the code

--- src/models/find_best_epoch.py
+++ src/models/find_best_epoch.py
@@ -9,7 +9,6 @@ from tqdm import tqdm
 from src import PROJECT_DIR
 
 log = logging.getLogger(__name__)
-log.setLevel(logging.INFO)
 
 
 @hydra.main(
@@ -30,28 +29,18 @@ def main(cfg: DictConfig) -> None:
     # Initialize an empty list to store DataFrame objects
     data_frames = []
 
-    # for model_name in tqdm(model_names, desc='Process model', unit=' models'):
-    for (model_name, fold), df_sample in tqdm(gb, desc='Process folds', unit=' folds'):
-        # Filter rows for training data
+    for _, df_sample in tqdm(gb, desc='Process folds', unit=' folds'):
         df_test = df_sample[(df_sample['Split'] == 'test') & (df_sample['Class'] == 'Mean')]
-
-        # Get the optimal epoch and its metrics
         max_f1_index = df_test['F1'].idxmax()
         best_epoch = df_test.loc[max_f1_index, 'Epoch']
-
-        # Filter rows for testing data on the best epoch
-        df_best_epoch_test = df_sample[
-            (df['Epoch'] == best_epoch) & (df['Model'] == model_name) & (df['Split'] == 'test')
+        df_best_epoch = df_sample[
+            (df_sample['Epoch'] == best_epoch) & (df_sample['Split'] == 'test')
         ]
-
-        # Pivot the dataframe to have classes as columns and F1 scores as values
-        df_fold = df_best_epoch_test.pivot(
+        df_fold = df_best_epoch.pivot(
             index=['Model', 'Fold', 'Epoch'],
             columns='Class',
             values='F1',
         )
-
-        # Reorder columns according to the specified header order
         header_order = [
             'Arteriole lumen',
             'Arteriole media',
@@ -65,17 +54,11 @@ def main(cfg: DictConfig) -> None:
             'Mean',
         ]
         df_fold = df_fold[header_order]
-
-        # Append DataFrame to the list
         data_frames.append(df_fold)
 
-    # Concatenate all DataFrames
     df_out = pd.concat(data_frames)
-
-    # Sort final dataframe
     df_out = df_out.sort_values(by=['Fold', 'Model'])
 
-    # Save the dataframe to a CSV file
     os.makedirs(save_dir, exist_ok=True)
     save_path = os.path.join(save_dir, 'best_metrics.xlsx')
     df_out.to_excel(save_path)
