f09f4281358f2f2fd2d0c04d1090d1b148cbeea9
Viacheslav Danilov <viacheslav.v.danilov@gmail.com>
2024-03-28 22:23:42 +0100

    [WiP]: refactoring
    [WiP]: refactoring

--- src/models/smp/predict.py
+++ src/models/smp/predict.py
@@ -1,3 +1,4 @@
+import json
 import logging
 import os
 import time
@@ -9,8 +10,9 @@ from omegaconf import DictConfig, OmegaConf
 from PIL import Image
 from tqdm import tqdm
 
-from src.models.smp.inference_img import prediction_model, processing_mask
+from src import PROJECT_DIR
 from src.models.smp.model import HistologySegmentationModel
+from src.models.smp.predict_image_DELETE_ME import prediction_model, processing_mask
 from src.models.smp.utils import preprocessing_img
 
 log = logging.getLogger(__name__)
@@ -18,15 +20,27 @@ log.setLevel(logging.INFO)
 
 
 @hydra.main(
-    config_path=os.path.join(os.getcwd(), 'configs'),
-    config_name='inference_batch',
+    config_path=os.path.join(PROJECT_DIR, 'configs'),
+    config_name='predict',
     version_base=None,
 )
 def main(cfg: DictConfig) -> None:
     log.warning(f'Config:\n\n{OmegaConf.to_yaml(cfg)}')
+
+    # Define absolute paths
+    data_dir = os.path.join(PROJECT_DIR, cfg.data_dir)
+    save_dir = os.path.join(PROJECT_DIR, cfg.save_dir)
+    model_dir = os.path.join(PROJECT_DIR, cfg.model_dir)
+
+    # TODO: check what you need and don't need from model_config
+    # Load the model with its configuration and weights
+    model_weights = os.path.join(model_dir, 'weights.ckpt')
+    with open(os.path.join(model_dir, 'config.json'), 'r') as file:
+        model_cfg = json.load(file)
+        print(model_cfg)  # TODO: remove it after debug
     start = time.time()
     model = HistologySegmentationModel.load_from_checkpoint(
-        cfg.model_weights,
+        checkpoint_path=model_weights,
         arch=cfg.architecture,
         encoder_name=cfg.encoder,
         model_name=cfg.model_name,
@@ -35,12 +49,15 @@ def main(cfg: DictConfig) -> None:
         map_location='cuda:0' if cfg.device == 'cuda' else cfg.device,
     )
     model.eval()
-    log.info(f'Model {cfg.model_name} loaded success, time: {time.time() - start}s')
+    log.info(
+        f'Model "{cfg.model_name}" loaded successfully. Time taken: {time.time() - start:.1f} s',
+    )
+
+    # Process images
     start_inference = time.time()
-    if not os.path.exists(cfg.save_dir):
-        os.makedirs(cfg.save_dir)
+    os.makedirs(save_dir, exist_ok=True)
     images_batch, images_input, images_name = [], [], []
-    images_path = glob(f'{cfg.data_dir}/*.[pj][np][ge]*')
+    images_path = glob(f'{data_dir}/*.[pj][np][ge]*')
     with tqdm(total=len(images_path), desc='Images predict') as pbar:
         for _, img_path in enumerate(images_path):
             images_batch.append(
@@ -55,20 +72,21 @@ def main(cfg: DictConfig) -> None:
             )
             images_name.append(os.path.basename(img_path).split('.')[0])
             if len(images_batch) == cfg.batch_size:
+                # TODO: think of this method instead -> model.predict(images)
                 masks = prediction_model(
                     model=model,
                     images=np.array(images_batch),
                     device=cfg.device,
                 )
                 for idx, (mask, image_input) in enumerate(zip(masks, images_input)):
-                    union_mask, color_mask = processing_mask(
+                    overlay, color_mask = processing_mask(
                         image_input=image_input,
                         input_size=cfg.input_size,
                         classes=cfg.classes,
                         mask=mask,
                     )
-                    color_mask.save(f'{cfg.save_dir}/{images_name[idx]}_color_mask.png')
-                    union_mask.save(f'{cfg.save_dir}/{images_name[idx]}_union_mask.png')
+                    color_mask.save(f'{save_dir}/{images_name[idx]}_mask.png')
+                    overlay.save(f'{save_dir}/{images_name[idx]}_overlay.png')
                 images_batch, images_input, images_name = [], [], []
                 pbar.update(cfg.batch_size)
         if len(images_input) != 0:
@@ -78,17 +96,17 @@ def main(cfg: DictConfig) -> None:
                 device=cfg.device,
             )
             for idx, (mask, image_input) in enumerate(zip(masks, images_input)):
-                union_mask, color_mask = processing_mask(
+                overlay, color_mask = processing_mask(
                     image_input=image_input,
                     input_size=cfg.input_size,
                     classes=cfg.classes,
                     mask=mask,
                 )
-                color_mask.save(f'{cfg.save_dir}/{images_name[idx]}_color_mask.png')
-                union_mask.save(f'{cfg.save_dir}/{images_name[idx]}_union_mask.png')
+                color_mask.save(f'{save_dir}/{images_name[idx]}_mask.png')
+                overlay.save(f'{save_dir}/{images_name[idx]}_overlay.png')
             pbar.update(len(images_input))
-    log.info(f'Inference time: {time.time() - start_inference}s')
-    log.info(f'Summary compute time: {time.time() - start}')
+    log.info(f'Prediction time: {time.time() - start_inference:.1f} s')
+    log.info(f'Overall computation time: {time.time() - start:.1f} s')
 
 
 if __name__ == '__main__':
