ffb5b62c2ca1df15f828c11eb31e3f8629b82680
Viacheslav Danilov <viacheslav.v.danilov@gmail.com>
2024-04-06 20:35:31 +0200

    [Fix]: add mandatory functions

--- src/data/convert_int_to_cv.py
+++ src/data/convert_int_to_cv.py
@@ -1,7 +1,9 @@
 import logging
 import os
+from pathlib import Path
 from typing import List, Tuple
 
+import cv2
 import hydra
 import numpy as np
 import pandas as pd
@@ -11,10 +13,70 @@ from sklearn.model_selection import KFold
 from tqdm import tqdm
 
 from src import PROJECT_DIR
-from src.data.convert_int_to_split import process_mask, process_metadata
+from src.data.mask_processor import MaskProcessor
+from src.data.utils import CLASS_COLOR, CLASS_ID, convert_base64_to_numpy
 
 log = logging.getLogger(__name__)
 log.setLevel(logging.INFO)
+
+
+def process_mask(
+    img_path: str,
+    df: pd.DataFrame,
+    smooth_mask: bool,
+    save_dir: str,
+) -> None:
+    img = cv2.imread(img_path)
+    img_height, img_width = img.shape[:2]
+    mask = np.zeros((img_height, img_width), dtype='uint8')
+    mask_color = np.zeros((img_height, img_width, 3), dtype='uint8')
+    mask_color[:, :] = (128, 128, 128)
+    mask_processor = MaskProcessor()
+    for _, row in df.iterrows():
+        obj_mask = convert_base64_to_numpy(row.encoded_mask).astype('uint8')
+        if smooth_mask:
+            obj_mask = mask_processor.smooth_mask(mask=obj_mask)
+            obj_mask = mask_processor.remove_artifacts(mask=obj_mask)
+        mask = build_mask(
+            mask=mask,
+            obj_mask=obj_mask,
+            class_id=CLASS_ID[row.class_name],  # type: ignore
+            origin=[row['x1'], row['y1']],
+        )
+        mask_color[mask == CLASS_ID[row.class_name]] = CLASS_COLOR[row.class_name]
+
+    img_stem = Path(img_path).stem
+    new_img_path = os.path.join(save_dir, 'img', f'{img_stem}.jpg')
+    mask_path = os.path.join(save_dir, 'mask', f'{img_stem}.png')
+    color_mask_path = os.path.join(save_dir, 'mask_color', f'{img_stem}.png')
+    cv2.imwrite(mask_path, mask)
+    cv2.imwrite(color_mask_path, mask_color)
+    cv2.imwrite(new_img_path, img, [cv2.IMWRITE_JPEG_QUALITY, 100])
+
+
+def build_mask(
+    mask: np.ndarray,
+    obj_mask: np.ndarray,
+    class_id: int,
+    origin: List[int],
+) -> np.ndarray:
+    obj_height, obj_width = obj_mask.shape
+    mask_new = np.zeros_like(mask)
+    mask_new[
+        origin[1] : origin[1] + obj_height,
+        origin[0] : origin[0] + obj_width,
+    ] = obj_mask[:, :]
+    mask[mask_new == 1] = class_id
+    return mask
+
+
+def process_metadata(
+    df: pd.DataFrame,
+    classes: List[str],
+) -> pd.DataFrame:
+    df = df[df['class_name'].isin(classes)]
+    df = df[df['area'] > 0]
+    return df
 
 
 def update_metadata(
